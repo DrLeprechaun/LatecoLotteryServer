e2a2457eac78923bf3841b56782087da6bb2918e
Vadim <leprechaunvanrosenkreuz@gmail.com>
2017-11-24 13:49:45 +0300

    Initial Commit

---
+++ src/main/java/com/lateco/lotteryserver/LatecoLotteryServerApplication.java
@@ -0,0 +1,12 @@
+package com.lateco.lotteryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatecoLotteryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatecoLotteryServerApplication.class, args);
+	}
+}
